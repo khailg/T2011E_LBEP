5c8dddd8d7c89ba3ec78b12b6c04631bf0f69b5a
khailg <74941957+khailg@users.noreply.github.com>
2020-11-25 18:13:42 +0700

    Nop bai tap buoi 1

---
+++ Demo 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+		// Dien A
+	int a;
+	printf("Nhap a= ");
+	scanf ("%d",&a);
+		// Dien B
+	int b;
+	printf("Nhap b= ");
+	scanf("%d",&b);
+		// So Sanh
+	if(a>b){	printf("Max: %d",a);
+	}else{		printf("Max: %d",b);
+	}
+}
